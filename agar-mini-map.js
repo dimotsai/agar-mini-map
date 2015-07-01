1e9c434a5cc760ff5aee6abfe9f2be44ce809169
Chih-Hao Tsai <a968574123@hotmail.com>
2015-07-01 16:53:32 +0800

    Force that mini-map-wrapper is on the foreground.

--- agar-mini-map.js
+++ agar-mini-map.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         agar-mini-map
 // @namespace    http://github.com/dimotsai/
-// @version      0.22
+// @version      0.23
 // @description  This script will show a mini map and your location on agar.io
 // @author       dimotsai
 // @license      MIT
@@ -335,5 +335,7 @@
 
     window.WebSocket.prototype = _WebSocket;
 
-    miniMapInit();
+    $(window.document).ready(function() {
+        miniMapInit();
+    })
 })();
